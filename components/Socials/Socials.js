11aab87e4322fb2f7706c6ad23a1111828922fa5
datejer <ejer.priv@wp.pl>
2021-06-04 23:54:05 +0200

    Organize images, add avatar frames for future use

--- components/Socials/Socials.js
+++ components/Socials/Socials.js
@@ -7,28 +7,28 @@ export default function Socials({ player }) {
 		<div className={styles.socials}>
 			{player.links.TWITTER ? (
 				<a href={player.links.TWITTER} className={styles.socialLink}>
-					<img src="/twitter.svg" className={styles.socialImg} />
+					<img src="/social/twitter.svg" className={styles.socialImg} />
 				</a>
 			) : (
 				''
 			)}
 			{player.links.YOUTUBE ? (
 				<a href={player.links.YOUTUBE} className={styles.socialLink}>
-					<img src="/youtube.svg" className={styles.socialImg} />
+					<img src="/social/youtube.svg" className={styles.socialImg} />
 				</a>
 			) : (
 				''
 			)}
 			{player.links.INSTAGRAM ? (
 				<a href={player.links.INSTAGRAM} className={styles.socialLink}>
-					<img src="/instagram.svg" className={styles.socialImg} />
+					<img src="/social/instagram.svg" className={styles.socialImg} />
 				</a>
 			) : (
 				''
 			)}
 			{player.links.TWITCH ? (
 				<a href={player.links.TWITCH} className={styles.socialLink}>
-					<img src="/twitch.svg" className={styles.socialImg} />
+					<img src="/social/twitch.svg" className={styles.socialImg} />
 				</a>
 			) : (
 				''
@@ -42,7 +42,7 @@ export default function Socials({ player }) {
 					style={{ cursor: 'pointer' }}
 				>
 					<a className={styles.socialLink}>
-						<img src="/discord.svg" className={styles.socialImg} />
+						<img src="/social/discord.svg" className={styles.socialImg} />
 					</a>
 				</Tooltip>
 			) : (
