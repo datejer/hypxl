db44015ac9d77edf7430eb9ac7e820ae31adee7b
datejer <ejer.priv@wp.pl>
2021-06-06 13:25:35 +0200

    Improve SEO

--- components/seo.js
+++ components/seo.js
@@ -1,13 +1,13 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function SEO({ title, description, favicon, ogimg }) {
-	const fullTitle = title ? `${title} – Hypxl` : "Hypxl";
+	const fullTitle = title ? `${title} – Hypxl` : 'Hypxl';
 
-	const fullDescription = description || "Hypixel statistics lookup.";
+	const fullDescription = description || 'Hypixel statistics lookup. 📊';
 
-	const fullIcon = favicon || "/logo.png";
+	const fullIcon = favicon || '/logo.png';
 
-	const fullOGImage = ogimg || "https://i.imgur.com/9CyqrJc.png";
+	const fullOGImage = ogimg || 'https://i.imgur.com/9CyqrJc.png';
 
 	return (
 		<Head>
@@ -23,11 +23,15 @@ export default function SEO({ title, description, favicon, ogimg }) {
 			<meta property="og:description" content={fullDescription} />
 			<meta property="og:image" content={fullOGImage} />
 
-			<meta property="twitter:card" content="summary_large_image" />
+			{/* <meta property="twitter:card" content="summary_large_image" /> */}
 			<meta property="twitter:url" content="https://hypxl.ga/" />
+			<meta property="twitter:domain" content="hypxl.ga" />
 			<meta property="twitter:title" content={title} />
 			<meta property="twitter:description" content={fullDescription} />
 			<meta property="twitter:image" content={fullOGImage} />
+
+			<meta property="og:site_name" content="Hypxl" />
+			<meta name="theme-color" content="#FCA96D" />
 		</Head>
 	);
 }
