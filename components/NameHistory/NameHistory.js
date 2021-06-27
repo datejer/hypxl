322240fab2c0eb9e2781e3b7a70ba2d648cc73bd
datejer <ejer.priv@wp.pl>
2021-06-27 12:31:09 +0200

    Style fixes with NameHistory

--- components/NameHistory/NameHistory.js
+++ components/NameHistory/NameHistory.js
@@ -4,14 +4,19 @@ import styles from './NameHistory.module.scss';
 
 export default function NameHistory({ names }) {
 	return (
-		<Game name="Name History">
-			<ol className={styles.list}>
-				{names.map((name, i, { length }) => (
-					<li key={name} style={i + 1 === length ? { fontWeight: '600' } : {}}>
-						{name}
-					</li>
-				))}
-			</ol>
-		</Game>
+		<div className={styles.nameHistory}>
+			<Game name="Name History">
+				<ol className={styles.list}>
+					{names.map((name, i, { length }) => (
+						<li
+							key={name}
+							style={i + 1 === length ? { fontWeight: '600' } : {}}
+						>
+							{name}
+						</li>
+					))}
+				</ol>
+			</Game>
+		</div>
 	);
 }
